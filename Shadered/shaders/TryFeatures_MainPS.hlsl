8ab917cfdcfcad9f4add6a413d62c373083ce859
RomainSepulchre <47340483+RomainSepulchre@users.noreply.github.com>
2026-02-13 17:59:55 -0500

    Experiment with render textures

--- Shadered/shaders/TryFeatures_MainPS.hlsl
+++ Shadered/shaders/TryFeatures_MainPS.hlsl
@@ -1,6 +1,8 @@
 uniform float2 mousePos;
 uniform Texture2D tex : register(t0);
 uniform SamplerState texSmp : register(s0);
+uniform Texture2D rTex : register(t1);
+uniform SamplerState rTexSmp : register(s1);
 uniform float4 varColor;
 
 struct PSInput
@@ -14,7 +16,11 @@ struct PSInput
 float4 main(PSInput pin) : SV_TARGET
 {
 	float4 texCol = tex.Sample(texSmp, pin.UV) * varColor;
+	float4 rTexCol = rTex.Sample(rTexSmp, pin.UV);
+	
 	pin.Color = float4(abs(pin.Normal.xyz), 1);
 	float2 absMousePos = abs(mousePos);
-	return pin.Color * float4(absMousePos.x, absMousePos.y, 1, 1) + texCol;
+	float4 col = pin.Color * float4(absMousePos.x, absMousePos.y, 1, 1) + texCol;
+	col -= rTexCol;
+	return col;
 }
