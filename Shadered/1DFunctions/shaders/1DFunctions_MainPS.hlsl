3abd2bb470ce8c59ba5af4682eb1fe07e3a0c5b1
RomainSepulchre <47340483+RomainSepulchre@users.noreply.github.com>
2026-02-14 22:01:44 -0500

    Experiment with a line function

--- Shadered/1DFunctions/shaders/1DFunctions_MainPS.hlsl
+++ Shadered/1DFunctions/shaders/1DFunctions_MainPS.hlsl
@@ -4,9 +4,23 @@ cbuffer vars : register(b0)
 	float uTime;
 };
 
+uniform float lineThickness = 1.0;
+uniform float4 linePosAB;
+uniform float2 linePosC;
+
 // -----------------
 // --- Functions ---
 // -----------------
+
+float lineSegment(float2 p, float2 a, float2 b, float thickness = 1.0)
+{
+	float2 pa = p - a;
+	float2 ba = b - a;
+	
+	float h = clamp(dot(pa,ba) / dot(ba,ba), 0.0, 1.0 );
+	
+	return 1 - smoothstep(0.0, thickness / uResolution.x, length(pa - ba * h));
+}
 
 
 // ****************
@@ -17,6 +31,11 @@ float4 main(float4 fragCoord : SV_POSITION) : SV_TARGET
 {
     float2 uv = fragCoord.xy/uResolution;
     
-    return float4(uv, 0.0, 1.0);
+    float line1 = lineSegment(uv, linePosAB.xy, linePosAB.zw, lineThickness);
+    float line2 = lineSegment(uv, linePosAB.zw.xy, linePosC, lineThickness);
+    float line3 = lineSegment(uv, linePosC, linePosAB.xy, lineThickness);
+    
+    return line1 + line2 + line3;
+    //return float4(uv, 0.0, 1.0);
 }
 
