cb4ad05497277af9392ca5e5450f2ec29d1af8fe
RomainSepulchre <47340483+RomainSepulchre@users.noreply.github.com>
2026-03-25 21:58:45 -0400

    Continue to decompose fire shader to understand how it works

--- Shadered/shaders/FireAndSmoke_FireAndSmokePS.hlsl
+++ Shadered/shaders/FireAndSmoke_FireAndSmokePS.hlsl
@@ -10,28 +10,28 @@ uniform float4 dbg;
 
 float4 smoke(float2 uv)
 {
-    float depth, s;
+    float depth, offset;
     float t = uTime;
     
-    float3 viewRes = float3(uResolution, 0.0);
+    float3 smokeUv = float3(uResolution, 0.0);
     
     float4 colorOut = float4(0);
      
-    for(float i = 0; i < 64.0; i ++,
-    	viewRes.y *= 0.6, // Reduce viewRes to change the scale at each new loop
-    	s = 0.3 + 0.2 * abs(viewRes.y - 2.0), // absolute dist of viewRes.y compared to 2.0, value is remaped to 0.3 + 20% of the absolute dist
-    	depth += s, // accumulate s at each new iteration (store total of all s distance calculated) 
-    	colorOut += 10.0 / s)
+    for(float i = 0; i < 64.0; i ++, // each new iteration add new density layer to the smoke
+    	smokeUv.y *= 0.6, // Reduce smokeUv to change the scale at each new loop
+    	offset = 0.3 + 0.2 * abs(smokeUv.y - 2.0), // absolute dist of smokeUv.y compared to 2.0, value is remaped to 0.3 + 20% of the absolute dist
+    	depth += offset, // accumulate offset at each new iteration (store total of all offset distance calculated) 
+    	colorOut += 10.0 / offset)
     {   
     	float j;
-    	viewRes = float3(uv * depth, depth - t * 10.0); // Reset viewRes to be the to get new value based on the uv position and depth
-    	for(j = .01; j < 4.; j += j)
+    	smokeUv = float3(uv * depth, depth - t * 10.0); // Reset smokeUv to be the to get new value based on the current depth
+    	for(j = 0.01; j < 4.0; j += j)
     	{
-    		// Deform viewRes value
-    		viewRes.yz -= cos(viewRes.zx * 0.05); // Create periodic ondulations
-    		float3 vecA = sin(0.02 * viewRes.z * j + 0.03 * viewRes.x + t + 0.5 * viewRes / j); // 0.02, 0.03 and 0.5 controls the smoke deformations
+    		// Deform smokeUv value
+    		smokeUv.yz -= cos(smokeUv.zx * 0.05); // Create periodic ondulations
+    		float3 vecA = sin(0.02 * smokeUv.z * j + 0.03 * smokeUv.x + t + 0.5 * smokeUv / j); // 0.02, 0.03 and 0.5 controls the smoke deformations
     		float3 vecB = float3(0.1 + j); // 0.1 control how much the smoke is stretch on Y axis
-          	viewRes.yz -= abs(dot(vecA, vecB)); // Non-linear deformation the depends on j value
+          	smokeUv.yz -= abs(dot(vecA, vecB)); // Non-linear deformation that depends on j value
     	}
     }
     
@@ -39,26 +39,41 @@ float4 smoke(float2 uv)
     return colorOut / 2000.0; 
 }
 
-float4 fire(float2 u)
+float4 fire(float2 uv)
 {
-    float i, d, s, n;
+    float depth, offset;
     float t = uTime;
-    float3 p;
-    float4 o = float4(0);
-    float2x2 r = float2x2(cos(.3*t+float4(0,33,11,0)));
     
-    for(; i++<64.; ) {
-        p = float3(u * d, d);
-        p += cos(p.z+t+p.yzx*.5)*.6;
-        s = p.y-2.;
-        p.yz *= r;
-        for (n = 1.6; n < 32.; n += n )
-            s += abs(dot(sin( p.z + t + p*n ), float3(2.5))) / n;
-        d += s = .01 + abs(s)*.1;
-        o += 1. / s;
+    float3 fireUv;
+    
+    float4 colorOut = float4(0);
+    float2x2 rotMatrix = float2x2(cos(0.3 * t + float4(0, 33, 11, 0)));
+    
+    for(float i=0; i < 64.0; i++) // each new iteration add new density layer to the fire
+    {
+        fireUv = float3(uv * depth, depth); // Reset fireUv to get new value based on the current depth
+        fireUv += cos(fireUv.z + t + fireUv.yzx * 0.5) * 0.6; // Add a periodic deformation of the UV to create ondulation
+        offset = fireUv.y - 2.0; // dist of fireUv.y compared to 2.0
+        fireUv.yz *= rotMatrix; // Apply the rotation matrix to simulate a swirling fire
+        
+        for (float j = 1.6; j < 32.0; j += j)
+        {
+        	// Deform offset value: Non-linear deformation that depends on j value
+        	offset += abs(dot(sin(fireUv.z + t + fireUv * j), float3(2.5))) / j;
+        }
+        
+        offset = 0.01 + abs(offset) * 0.1; // Get the absolute value of the offset and reduce it
+        depth += offset; // accumulate offset at each new iteration (store total of all offset distance calculated) 
+        colorOut += 1.0 / offset;
     }
     
-    return float4(6,2,1,1) * o * o / d / 2e5;
+    float4 fireColor = float4(6,2,1,1);
+    
+    // Return final color
+    // -> Apply fire color
+    // -> colorOut * colorOut: to accentuate bright spot of the fire (we could multiply by a huge value like 1e3 to have more neutral color)
+    // -> we divide by depth to equlibrate the intensity then by 2e5 to normalize the color value
+    return fireColor * colorOut * colorOut / depth / 2e5;
 }
 
 float4 main(float4 fragCoord : SV_POSITION) : SV_TARGET
@@ -67,9 +82,12 @@ float4 main(float4 fragCoord : SV_POSITION) : SV_TARGET
     float2 uv = (fragCoord.xy - uResolution.xy / 2.0) / uResolution.y;
     
     float4 color = lerp(fire(uv), smoke(uv), 0.92);
-	color = tanh(color); // tanh ?????
+    // tanh = hyperbolic tangent of a value
+    // -> we use tanh() to bring back the colors in a more standard interval
+    // 		-> Reduce high value and overexposed zone, increase shadows and keep details in the medium value
+	color = tanh(color);
 	
-	color = smoke(uv);
+	color = lerp(fire(uv), 0.0, 0.92);
     
     return color;
 }
